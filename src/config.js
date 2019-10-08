7d44b58855816d6cc3bd323f9af4fcef5623cec6
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-10-08 17:16:13 -0400

    update config to reference API_URL from heroku config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,3 @@
-const URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://noteful-server-jamesj.herokuapp.com'
-    : 'http://localhost:8022'
-
-console.log(process.env.NODE_ENV)
 export default {
-  API_ENDPOINT: URL,
+  API_ENDPOINT: process.env.API_URL || 'http://localhost:8022',
 }
