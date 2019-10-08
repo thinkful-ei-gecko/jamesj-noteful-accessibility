3c612a8d50d19a567f94f81501ec3d2a96293a31
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-10-08 17:08:20 -0400

    console log on URL

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
-const URL = process.env.NODE_ENV === 'production' ? 'https://noteful-server-jamesj.herokuapp.com' : 'http://localhost:8022'
+const URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://noteful-server-jamesj.herokuapp.com'
+    : 'http://localhost:8022'
+
+console.log(URL)
 export default {
-  API_ENDPOINT: URL
+  API_ENDPOINT: URL,
 }
